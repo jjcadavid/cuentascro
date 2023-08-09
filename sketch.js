d6b771bbc620b9898af366a96a292f992ba73727
jjcadavid <93051977+jjcadavid@users.noreply.github.com>
2023-08-09 09:14:57 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,23 +11,11 @@ function setup() {
   //createCanvas(windowWidth, windowHeight);
   createCanvas(1920, 1080);
   background(255);
-  numpreguntas = tabla.getRowCount();
-  for (let i = 0; i < numpreguntas; i++) {
-    //tabla.rows[i].arr[0];
-    //tabla.rows[i].arr[5];
 
-    for (let j = 0; j < 4; j++) {
-      //listadoPreguntas[i].respuestas[j] = tabla.rows[i].arr[j + 1];
-    }
-  }
-  print(tabla.rows[0].arr[0]);
-  //text(tabla.rows[0].arr[5],0,20);
-  //print(listadoPreguntas);
-    tabla = loadTable("/Cuentas/ingresosCRO.csv", "csv", "header");
-  imagenIngresos = loadImage("/Imagenes/IngresosCRO.PNG");
-  imagenEgresos = loadImage("Imagenes/egresoCRO.PNG");
-  logo = loadImage("Imagenes/logo2.png");
-  saldo=loadImage("Imagenes/saldoCRO.PNG");
+  imagenIngresos = loadImage("IngresosCRO.PNG");
+  imagenEgresos = loadImage("egresoCRO.PNG");
+  logo = loadImage("logo2.png");
+  saldo=loadImage("saldoCRO.PNG");
 }
 
 function draw() {
@@ -37,9 +25,3 @@ function draw() {
   image(saldo,width * 0.27, height * 0.65);
 }
 
-function draw() {
-  image(logo,-width*0.1,height*0.02,width*0.5,height*0.4);
-  image(imagenIngresos, width * 0.05, height * 0.15);
-  image(imagenEgresos, width * 0.05, height * 0.4);
-  image(saldo,width * 0.27, height * 0.65);
-}
