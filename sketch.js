a0b6a73befca5f9e4a01a8e46b76b2d4150320c8
jjcadavid <93051977+jjcadavid@users.noreply.github.com>
2023-08-15 15:08:59 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,12 +10,13 @@ function setup() {
   createCanvas(1920, 1080);
   background(255);
 
-  imagen = loadImage("CUENTRAScro15082023.png");
+  imagen = loadImage("CUENTRAS cro15082023.png");
   logo = loadImage("logo2.png");
 }
 
 function draw() {
   image(logo,-width*0.1,height*0.02,width*0.5,height*0.4);
-  image(imagen, width * 0.05, height * 0.15);
+  imageMode(CENTER);
+  image(imagen, width * 0.5, height * 0.5);
 }
 
