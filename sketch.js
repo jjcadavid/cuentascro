cdc2a9c59cc0f5525f2884ed3a7f52d4f433764f
jjcadavid <93051977+jjcadavid@users.noreply.github.com>
2023-08-15 14:54:24 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,8 @@
 let table;
 let numpreguntas;
 let listadoPreguntas = [];
-let imagenIngresos;
-let imagenEgresos;
+let imagen;
 let logo;
-let saldo;
 
 
 function setup() {
@@ -12,16 +10,12 @@ function setup() {
   createCanvas(1920, 1080);
   background(255);
 
-  imagenIngresos = loadImage("IngresosCRO.PNG");
-  imagenEgresos = loadImage("egresoCRO.PNG");
+  imagen = loadImage("cuentras CRO15082023.png");
   logo = loadImage("logo2.png");
-  saldo=loadImage("saldoCRO.PNG");
 }
 
 function draw() {
   image(logo,-width*0.1,height*0.02,width*0.5,height*0.4);
-  image(imagenIngresos, width * 0.05, height * 0.15);
-  image(imagenEgresos, width * 0.05, height * 0.4);
-  image(saldo,width * 0.27, height * 0.65);
+  image(imagen, width * 0.05, height * 0.15);
 }
 
